f8aec4be698aa57389ea286ac29a89ff545fb0b1
planetabhi <theplanetabhi@gmail.com>
2025-01-05 17:26:38 +0530

    ..

--- src/components/PercentageCalculator.tsx
+++ src/components/PercentageCalculator.tsx
@@ -112,15 +112,54 @@ const PercentageCalculator = () => {
               </button>
             </div>
             <div className="result">
-              <div>Result: {(percentageResult ?? 0).toFixed(2)}</div>
-              {percentageResult !== null && (
+              <div className="result-text">
                 <div>
-                  {percentageResult.toFixed(2)} is {percentageInput1 ?? 0}% of {percentageInput2 ?? 0}.
+                  Result: <span className="result-value">{(percentageResult ?? 0).toFixed(2)}</span>
                 </div>
-              )}
+                {percentageResult !== null && (
+                  <div className="result-secondary-text">
+                    {percentageResult.toFixed(2)} is {percentageInput1 ?? 0}% of {percentageInput2 ?? 0}.
+                  </div>
+                )}
+              </div>
               {percentageResult !== null && (
                 <button className="clear-button" onClick={clearInputs}>
-                  Clear
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 256 256"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                  >
+                    <rect width="256" height="256" fill="none" />
+                    <path
+                      d="M112,224a95.2,95.2,0,0,1-29-48"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="16"
+                    />
+                    <path
+                      d="M192,152c0,31.67,13.31,59,40,72H61A103.65,103.65,0,0,1,32,152c0-28.21,11.23-50.89,29.47-69.64a8,8,0,0,1,8.67-1.81L95.52,90.83a16,16,0,0,0,20.82-9l21-53.11c4.15-10,15.47-15.32,25.63-11.53a20,20,0,0,1,11.51,26.4L153.13,96.69a16,16,0,0,0,8.93,20.76L187,127.29a8,8,0,0,1,5,7.43Z"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="16"
+                    />
+                    <line
+                      x1="43.93"
+                      y1="105.57"
+                      x2="192.8"
+                      y2="165.12"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="16"
+                    />
+                  </svg>
                 </button>
               )}
             </div>
@@ -149,15 +188,54 @@ const PercentageCalculator = () => {
               </button>
             </div>
             <div className="result">
-              <div>Result: {(rateResult ?? 0).toFixed(2)}%</div>
-              {rateResult !== null && (
+              <div className="result-text">
                 <div>
-                  {rateInput1 ?? 0} is {(rateResult ?? 0).toFixed(2)}% of {rateInput2 ?? 0}.
+                  Result: <span className="result-value">{(rateResult ?? 0).toFixed(2)}%</span>
                 </div>
-              )}
+                {rateResult !== null && (
+                  <div className="result-secondary-text">
+                    {rateInput1 ?? 0} is {(rateResult ?? 0).toFixed(2)}% of {rateInput2 ?? 0}.
+                  </div>
+                )}
+              </div>
               {rateResult !== null && (
                 <button className="clear-button" onClick={clearInputs}>
-                  Clear
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 256 256"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                  >
+                    <rect width="256" height="256" fill="none" />
+                    <path
+                      d="M112,224a95.2,95.2,0,0,1-29-48"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="16"
+                    />
+                    <path
+                      d="M192,152c0,31.67,13.31,59,40,72H61A103.65,103.65,0,0,1,32,152c0-28.21,11.23-50.89,29.47-69.64a8,8,0,0,1,8.67-1.81L95.52,90.83a16,16,0,0,0,20.82-9l21-53.11c4.15-10,15.47-15.32,25.63-11.53a20,20,0,0,1,11.51,26.4L153.13,96.69a16,16,0,0,0,8.93,20.76L187,127.29a8,8,0,0,1,5,7.43Z"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="16"
+                    />
+                    <line
+                      x1="43.93"
+                      y1="105.57"
+                      x2="192.8"
+                      y2="165.12"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="16"
+                    />
+                  </svg>
                 </button>
               )}
             </div>
@@ -190,15 +268,54 @@ const PercentageCalculator = () => {
               </button>
             </div>
             <div className="result">
-              <div>Result: {(baseResult ?? 0).toFixed(2)}</div>
-              {baseResult !== null && (
+              <div className="result-text">
                 <div>
-                  {baseInput1 ?? 0} is {baseInput2 ?? 0}% of {(baseResult ?? 0).toFixed(2)}.
+                  Result: <span className="result-value">{(baseResult ?? 0).toFixed(2)}</span>
                 </div>
-              )}
+                {baseResult !== null && (
+                  <div className="result-secondary-text">
+                    {baseInput1 ?? 0} is {baseInput2 ?? 0}% of {(baseResult ?? 0).toFixed(2)}.
+                  </div>
+                )}
+              </div>
               {baseResult !== null && (
                 <button className="clear-button" onClick={clearInputs}>
-                  Clear
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 256 256"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                  >
+                    <rect width="256" height="256" fill="none" />
+                    <path
+                      d="M112,224a95.2,95.2,0,0,1-29-48"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="16"
+                    />
+                    <path
+                      d="M192,152c0,31.67,13.31,59,40,72H61A103.65,103.65,0,0,1,32,152c0-28.21,11.23-50.89,29.47-69.64a8,8,0,0,1,8.67-1.81L95.52,90.83a16,16,0,0,0,20.82-9l21-53.11c4.15-10,15.47-15.32,25.63-11.53a20,20,0,0,1,11.51,26.4L153.13,96.69a16,16,0,0,0,8.93,20.76L187,127.29a8,8,0,0,1,5,7.43Z"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="16"
+                    />
+                    <line
+                      x1="43.93"
+                      y1="105.57"
+                      x2="192.8"
+                      y2="165.12"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="16"
+                    />
+                  </svg>
                 </button>
               )}
             </div>
